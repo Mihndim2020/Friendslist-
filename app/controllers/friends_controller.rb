40f28f90a31362f4c4e9fb7c5b6aebf9d2712e1f
Mihndim <jelsieprisca2016@gmail.com>
2021-04-14 12:49:20 -0400

    Add edit, update methods and views

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,6 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, except: [:index, :show]
   # GET /friends or /friends.json
   def index
     @friends = Friend.all
